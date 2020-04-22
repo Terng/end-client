09726c16d042a5b389706784a4d16452aa6faea4
Pongdilok Ketngam <Terng.pongdilok@gmail.com>
2020-04-22 14:17:08 +0700

    Update Proxy, Url

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -9,7 +9,7 @@ import { ApolloProvider as ApolloHooksProvider } from "@apollo/react-hooks";
 
 const cache = new InMemoryCache();
 const link = new HttpLink({
-  uri: "http://localhost:5000/",
+  uri: "https://arcane-plateau-26527.herokuapp.com/",
 });
 const client = new ApolloClient({
   cache,
